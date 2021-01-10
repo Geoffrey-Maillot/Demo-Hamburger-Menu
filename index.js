0a62c7108b432910684145bd0b7e515a2f301064
Geoffrey-Maillot <geoffrey.maillot@gmail.com>
2021-01-10 22:55:36 +0100

    menu hamburger de base ok

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log(`allo`)
+const bodyElement = document.querySelector(`body`)
+const menuElement = document.getElementById(`menu`)
+const iconElement = document.getElementById(`header__icon`)
+
+iconElement.addEventListener(`click`, (e) => {
+  
+  menuElement.classList.toggle(`visible`);
+  iconElement.classList.toggle(`visible-icon`)
+  bodyElement.classList.toggle(`hidden-scroll`)
+  
+})
